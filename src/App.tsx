dc26d7658306447793612783dd9aa04cc1570076
wangzk <zekai@teiron-inc.cn>
2024-04-25 19:16:27 +0800

    feat: react-photo-view 测试

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import "./App.css";
-import UploadFileList from "./page/UploadFileList";
+import "./App.css"
+import UploadFileList from "./page/UploadFileList"
+import "react-photo-view/dist/react-photo-view.css"
+import PhotoList from "./page/PhotoList"
 
 function App() {
   return (
     <>
       <UploadFileList />
+      <PhotoList />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
