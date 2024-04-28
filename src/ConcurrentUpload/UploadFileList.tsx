d86fe44be8a9c714e739f4e177c5eec47c61d2e6
wangzk <zekai@teiron-inc.cn>
2024-04-28 21:30:29 +0800

    feat: 调整文件上传的目录结构

--- src/ConcurrentUpload/UploadFileList.tsx
+++ src/ConcurrentUpload/UploadFileList.tsx
@@ -1,10 +1,10 @@
-import { Progress } from "@douyinfe/semi-ui"
-import { useUploadQueue } from "./useUploadQueue"
-import { TaskStatusEnumMap } from "./types"
-import { formatRemainingTime } from "./utils"
+import { Progress } from "@douyinfe/semi-ui";
+import { useUploadQueue } from "./hooks/useUploadQueue";
+import { TaskStatusEnumMap } from "./types";
+import { formatRemainingTime } from "../page/utils";
 
 const UploadFileList = () => {
-  const { taskQueue, uploadFile, abortAll } = useUploadQueue()
+  const { taskQueue, uploadFile, abortAll } = useUploadQueue();
 
   return (
     <div>
@@ -29,7 +29,7 @@ const UploadFileList = () => {
               <div>
                 <button
                   onClick={() => {
-                    abortAll(t.uid)
+                    abortAll(t.uid);
                   }}
                 >
                   取消全部
@@ -78,7 +78,7 @@ const UploadFileList = () => {
         ))}
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default UploadFileList
+export default UploadFileList;
